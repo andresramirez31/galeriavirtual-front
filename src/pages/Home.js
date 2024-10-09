ce262d61f0499e48ae78268dc0204bf374af22d5
andresramirez31 <andres.ramirez31@udea.edu.co>
2024-10-08 22:05:51 -0500

    Adicion estilo a pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles/main.css';
+import '../styles/Home.css';
 
 const Home = () => {
     return (
       <div>
-      <h1>GALERIA VIRTUAL UDEA</h1>
+      <h2>GALERIA VIRTUAL UDEA</h2>
       <p>Para exhibir tus proyectos academicos al mundo!</p>
+      
+      <div className="HomeBox">
 
-      <Link to="/createGallery">
-        <button style={{ padding: '10px 20px', fontSize: '16px' }}>
-          Ir a la creacion de salon
-        </button>
-      </Link>
+        <h3 className="Greeting">Bienvenido usuario/a</h3>
 
-      <Link to="/login">
-        <button style={{ padding: '10px 20px', fontSize: '16px' }}>
-          Iniciar Sesion
-        </button>
-      </Link>
+        
+
+          <Link to="/createGallery">
+            <button className='btn_event' style={{ padding: '10px 20px', fontSize: '16px' }}>
+              Ir a la creacion de salon
+            </button>
+          </Link>
+
+          <Link to="/login">
+            <button className="btn_event" style={{ padding: '10px 20px', fontSize: '16px' }}>
+              Iniciar Sesion
+            </button>
+          </Link>
+
+      </div>
     </div>
     );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     
     // Prueba simulada, ajustar API
-    console.log('Logging in with', email, password);
+    console.log('Logging in with', email, username, password);
     
     
     navigate('/');
@@ -18,30 +20,47 @@ const Login = () => {
 
   return (
     <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input 
-            type="email"
-            id="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input 
-            type="password"
-            id="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <h2>Inicio de sesion</h2>
+      <div className='LoginBox'>
+        <form className='form' onSubmit={handleSubmit}>
+          <div>
+            <label className="label_box_login" htmlFor='email'>Correo Electronico:</label>
+            <input 
+              type="email"
+              id="email"
+              className='value_box' 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+            />
+          </div>
+
+          <div>
+            <label className="label_box_login" htmlFor='username'>Usuario:</label>
+            <input 
+              type="text"
+              id="username" 
+              className='value_box'
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+            />
+          </div>
+
+          <div>
+            <label className="label_box_login" htmlFor='password'>Contraseña:</label>
+            <input 
+              type="password"
+              id="password"
+              className='value_box' 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit" className='save_button'>Login</button>
+        </form>
+      </div>
     </div>
   );
 };
